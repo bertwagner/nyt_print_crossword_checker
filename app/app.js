f526cdf4125d36574f0891b51703e5f3ab3e92ff
Bert Wagner <bertwagner@bertwagner.com>
2021-11-09 20:17:48 -0500

    functional camera on page

---
+++ app/app.js
@@ -0,0 +1,13 @@
+// Prefer camera resolution nearest to 1280x720.
+var constraints = { audio: false, video: { width: 640, height: 360 } };
+
+navigator.mediaDevices.getUserMedia(constraints)
+.then(function(mediaStream) {
+  var video = document.querySelector('video');
+  video.srcObject = mediaStream;
+  video.onloadedmetadata = function(e) {
+    video.play();
+  };
+})
+.catch(function(err) { console.log(err.name + ": " + err.message); }); // always check for errors at the end.
+
