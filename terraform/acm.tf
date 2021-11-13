505cc757760b62b5931d4992878a1450956bb4a1
Bert Wagner <bertwagner@bertwagner.com>
2021-11-13 11:12:29 -0500

    add cloudfront distribution for s3

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -1,5 +1,6 @@
 resource "aws_acm_certificate" "cert" {
   domain_name       = "crosschecker.app"
+  subject_alternative_names = ["*.crosschecker.app"]
   validation_method = "DNS"
 
   lifecycle {
@@ -12,16 +13,3 @@ resource "aws_acm_certificate_validation" "cert" {
   validation_record_fqdns = [for record in aws_route53_record.cname_validation: record.fqdn]
 }
 
-resource "aws_acm_certificate" "cert2" {
-  domain_name       = "*.crosschecker.app"
-  validation_method = "DNS"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_acm_certificate_validation" "cert2" {
-  certificate_arn         = aws_acm_certificate.cert2.arn
-  validation_record_fqdns = [for record in aws_route53_record.cname_validation2: record.fqdn]
-}
