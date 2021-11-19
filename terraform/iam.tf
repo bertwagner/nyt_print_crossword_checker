c70c3758ed8eb060d106017ac5efa27a2a1c311f
Bert Wagner <bertwagner@bertwagner.com>
2021-11-18 21:16:56 -0500

    adds s3 upload bucket

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,13 +1,30 @@
-resource "aws_iam_user" "CrossCheckerServiceUser" {
-  name = "CrossCheckerServiceUser"
+# User for uploading/downloading Terraform state via backened
+resource "aws_iam_user" "terraform_state" {
+  name = "crosschecker-terraform-state-user"
 }
 
-resource "aws_iam_policy" "policy" {
-  name = "crosschecker-service-policy"
-  policy = file("policies/iam-crosschecker-service.json")
+resource "aws_iam_policy" "terraform_state" {
+  name = "crosschecker-terraform-state-policy"
+  policy = file("policies/iam-terraform-state.json")
 }
 
-resource "aws_iam_user_policy_attachment" "policy-attach" {
-  user = aws_iam_user.CrossCheckerServiceUser.name
-  policy_arn = aws_iam_policy.policy.arn
+resource "aws_iam_user_policy_attachment" "terraform_state" {
+  user = aws_iam_user.terraform_state.name
+  policy_arn = aws_iam_policy.terraform_state.arn
+}
+
+
+# User for uploading images to S3 bucket
+resource "aws_iam_user" "image_uploader" {
+  name = "crosschecker-image-uploader-user"
+}
+
+resource "aws_iam_policy" "image_uploader" {
+  name = "crosschecker-image-uploader-policy"
+  policy = file("policies/iam-image-uploader.json")
+}
+
+resource "aws_iam_user_policy_attachment" "image_uploader" {
+  user = aws_iam_user.image_uploader.name
+  policy_arn = aws_iam_policy.image_uploader.arn
 }
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,8 +1,18 @@
+# Keeps track of Terraform State
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "crosschecker.app-state"
+  acl = "private"
+  versioning {
+    enabled = true
+  }
+}
+
+
+#Bucket for hosting the static web app
 resource "aws_s3_bucket" "crosschecker_app" {
   bucket = "crosschecker.app"
   acl    = "public-read"
   policy = file("policies/s3-public-website.json")
-
 
   website {
     index_document = "index.html"
@@ -10,10 +20,9 @@ resource "aws_s3_bucket" "crosschecker_app" {
   }
 }
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "crosschecker.app-state"
+
+# Unprocessed user-uploaded crossword images
+resource "aws_s3_bucket" "crosschecker_app_uploads" {
+  bucket = "crosschecker.app-uploads"
   acl = "private"
-  versioning {
-    enabled = true
-  }
 }
