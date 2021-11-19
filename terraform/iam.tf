2f9af3d341784beb18231eb8654097aae321880e
Bert Wagner <bertwagner@bertwagner.com>
2021-11-19 07:27:32 -0500

    use jsonencode()

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -33,19 +33,17 @@ resource "aws_iam_user_policy_attachment" "image_uploader" {
 resource "aws_iam_role" "iam_for_lambda" {
   name = "iam_for_lambda"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
+  assume_role_policy = jsonencode({
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Action": "sts:AssumeRole",
+          "Principal": {
+            "Service": "lambda.amazonaws.com"
+          },
+          "Effect": "Allow",
+          "Sid": ""
+        }
+      ]
+    })
 }
