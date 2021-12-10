9ff1ecfee6354c96edd96ef735a99f4618ed668b
Bert Wagner <bertwagner@bertwagner.com>
2021-12-10 14:35:29 -0500

    init modularity

--- ml_model/src/data/make_dataset.py
+++ ml_model/src/data/make_dataset.py
@@ -15,7 +15,6 @@ def main(input_filepath, output_filepath):
     for file in os.scandir(input_filepath):
         print(file.path)
 
-
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
