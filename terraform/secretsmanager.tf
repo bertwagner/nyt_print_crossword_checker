aaa990c5a63b839b44b2627f201a260a757f7f90
Bert Wagner <bertwagner@bertwagner.com>
2021-12-09 21:12:40 -0500

    add new secret

--- terraform/secretsmanager.tf
+++ terraform/secretsmanager.tf
@@ -1,3 +1,6 @@
 resource "aws_secretsmanager_secret" "iam-image-uploader-secret" {
   name = "crosschecker-image-uploader-user-access-key"
 }
+resource "aws_secretsmanager_secret" "nyt-login-credentials" {
+  name = "crosschecker-nyt-login"
+}
