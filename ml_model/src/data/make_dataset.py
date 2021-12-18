897f3d12fce81efd510011c8ba90c93a611caf63
Bert Wagner <bertwagner@bertwagner.com>
2021-12-18 14:00:02 -0500

    refactor into methods

--- ml_model/src/data/make_dataset.py
+++ ml_model/src/data/make_dataset.py
@@ -2,36 +2,27 @@ import logging
 from pathlib import Path
 import os
 from src.data import image_proccessor as ip
+from src.data import crossword_downloader as cd
 import datetime
 import cv2
 import shutil
+import json
+
 
 def main(input_filepath, output_filepath,delete_files):
 
-    warped_output_path = os.path.join(output_filepath,"warped")
+    image_input_path = os.path.join(input_filepath,"image_uploads")
+    image_output_path = os.path.join(output_filepath,"image_uploads")
+    crossword_input_path = os.path.join(input_filepath,"nyt_answer_keys")
+    crossword_output_path = os.path.join(output_filepath,"nyt_answer_keys")
+
+    warped_output_path = os.path.join(image_output_path,"warped")
     __make_folder_if_not_exists(warped_output_path,delete_files)
+    __make_folder_if_not_exists(crossword_output_path,delete_files)
 
-    for file in os.scandir(input_filepath):
-        puzzle_date = datetime.datetime.strptime(file.name[0:10],'%Y-%m-%d').date()
-        image_raw = cv2.imread(os.path.join(input_filepath,file.name))
-        
-        # warp and save image
-        processor = ip.ImageProcessor(image_raw)
-        processor.warp_and_transform()
-       
-        
-        cv2.imwrite(os.path.join(warped_output_path,file.name),processor.image['warped'])
-
-        # slice and save individual cell images
-        processor.slice_up_grid()
-
-        # make image folder if it doesn't exist
-        output_folder = os.path.join(output_filepath,"cells",os.path.splitext(file.name)[0])
-        __make_folder_if_not_exists(output_folder,delete_files)
-        
-        # save all cells as individual images
-        for i,cell in enumerate(processor.image['cells']):
-            cv2.imwrite(os.path.join(output_folder,f"{str(i)}.png"),cell)
+    for file in os.scandir(image_input_path):
+        __process_image(file,image_input_path,warped_output_path,image_output_path)
+        __process_crossword(file,crossword_input_path,crossword_output_path)
 
 def __make_folder_if_not_exists(folder_path,delete_files):
     path_exists = os.path.exists(folder_path)
@@ -42,13 +33,42 @@ def __make_folder_if_not_exists(folder_path,delete_files):
 
     if not path_exists:
         os.makedirs(folder_path)
-        
+
+def __process_image(file,image_input_path,warped_output_path,image_output_path):
+    puzzle_date = datetime.datetime.strptime(file.name[0:10],'%Y-%m-%d').date()
+    image_raw = cv2.imread(os.path.join(image_input_path,file.name))
+    
+    # warp and save image
+    processor = ip.ImageProcessor(image_raw)
+    processor.warp_and_transform()
+    
+    
+    cv2.imwrite(os.path.join(warped_output_path,file.name),processor.image['warped'])
+
+    # slice and save individual cell images
+    processor.slice_up_grid()
+
+    # make image folder if it doesn't exist
+    output_folder = os.path.join(image_output_path,"cells",os.path.splitext(file.name)[0])
+    __make_folder_if_not_exists(output_folder,delete_files)
+    
+    # save all cells as individual images
+    for i,cell in enumerate(processor.image['cells']):
+        cv2.imwrite(os.path.join(output_folder,f"{str(i).rjust(4,'0')}.png"),cell)
+
+def __process_crossword(file,crosswords_input_path,crossword_output_path):
+    date = datetime.datetime.strptime(file.name[0:10],'%Y-%m-%d').date()
+
+    downloader = cd.CrosswordDownloader()
+    answer_key = downloader.get_answer_key(date)
+    with open(os.path.join(crossword_output_path,f"{date}.json"), "w") as text_file:
+        text_file.write(json.dumps(answer_key))
 
 if __name__ == '__main__':
     project_dir = Path(__file__).resolve().parents[2]
 
-    input_filepath = os.path.join(project_dir,"data/raw/image_uploads")
-    output_filepath = os.path.join(project_dir,"data/processed/image_uploads")
+    input_filepath = os.path.join(project_dir,"data/raw")
+    output_filepath = os.path.join(project_dir,"data/processed")
 
     delete_files = True
 
