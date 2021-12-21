c01ff8fa002f9c24f377ca154cec14421eb08494
Bert Wagner <bertwagner@bertwagner.com>
2021-12-20 20:07:39 -0500

    saves images to letter named folders

--- ml_model/src/data/make_dataset.py
+++ ml_model/src/data/make_dataset.py
@@ -21,8 +21,9 @@ def main(input_filepath, output_filepath,delete_files):
     __make_folder_if_not_exists(crossword_output_path,delete_files)
 
     for file in os.scandir(image_input_path):
-        __process_image(file,image_input_path,warped_output_path,image_output_path)
-        __process_crossword(file,crossword_input_path,crossword_output_path)
+        answer_key = __process_crossword(file,crossword_input_path,crossword_output_path)
+        __process_image(file,image_input_path,warped_output_path,image_output_path,answer_key)
+        
 
 def __make_folder_if_not_exists(folder_path,delete_files):
     path_exists = os.path.exists(folder_path)
@@ -34,7 +35,7 @@ def __make_folder_if_not_exists(folder_path,delete_files):
     if not path_exists:
         os.makedirs(folder_path)
 
-def __process_image(file,image_input_path,warped_output_path,image_output_path):
+def __process_image(file,image_input_path,warped_output_path,image_output_path,answer_key):
     puzzle_date = datetime.datetime.strptime(file.name[0:10],'%Y-%m-%d').date()
     image_raw = cv2.imread(os.path.join(image_input_path,file.name))
     
@@ -56,6 +57,12 @@ def __process_image(file,image_input_path,warped_output_path,image_output_path):
     for i,cell in enumerate(processor.image['cells']):
         cv2.imwrite(os.path.join(output_folder,f"{str(i).rjust(4,'0')}.png"),cell)
 
+        # save the individual cell to the letter folder it's supposed to go to
+        letter_output_folder = os.path.join(image_output_path,"letters/",str(answer_key[i]))
+        __make_folder_if_not_exists(letter_output_folder,False)
+        letter_file_path = os.path.join(letter_output_folder, f"{os.path.splitext(file.name)[0]}-{str(i).rjust(4,'0')}.png")
+        cv2.imwrite(letter_file_path, cell)
+
 def __process_crossword(file,crosswords_input_path,crossword_output_path):
     date = datetime.datetime.strptime(file.name[0:10],'%Y-%m-%d').date()
 
@@ -63,12 +70,14 @@ def __process_crossword(file,crosswords_input_path,crossword_output_path):
     answer_key = downloader.get_answer_key(date)
     with open(os.path.join(crossword_output_path,f"{date}.json"), "w") as text_file:
         text_file.write(json.dumps(answer_key))
+    
+    return answer_key
 
 if __name__ == '__main__':
     project_dir = Path(__file__).resolve().parents[2]
 
     input_filepath = os.path.join(project_dir,"data/raw")
-    output_filepath = os.path.join(project_dir,"data/processed")
+    output_filepath = os.path.join(project_dir,"data/interim")
 
     delete_files = True
 
