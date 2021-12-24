6e3609e92096d411abdade1ac87c646872200ef1
Bert Wagner <bertwagner@bertwagner.com>
2021-12-23 20:12:43 -0500

    render images in app

--- app/data_labeler/app.py
+++ app/data_labeler/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory, request, redirect
 from pathlib import Path
 import os
 
@@ -7,8 +7,30 @@ app = Flask(__name__)
 @app.route("/")
 def label_data():
     images_to_label = __identify_unverified_files("ml_model/data/interim/image_uploads/letters","ml_model/data/processed/image_uploads/letters")
+    
+    return render_template("label_data.html",image_list = images_to_label[:10])
 
-    return render_template("label_data.html",image_list = images_to_label)
+
+# We use this hack to serve images from outside the flask project directory
+@app.route('/img/<path:path>')
+def send_img(path):
+    project_dir = Path(__file__).resolve().parents[2]
+    input_images = os.path.join(project_dir,"ml_model/data/interim/image_uploads/letters")
+    return send_from_directory(input_images, path)
+
+@app.route('/save_labels', methods=['post'])
+def save_labels():
+    items = request.form
+
+    for item in items:
+        print("new item")
+        print(item)
+        print(request.form[item])
+        # each item comes in as original_1 and then next item is corrected_1
+
+    # TODO: move files whose labels have changed
+
+    return redirect('/')
 
 def __identify_unverified_files(input,output):
     project_dir = Path(__file__).resolve().parents[2]
