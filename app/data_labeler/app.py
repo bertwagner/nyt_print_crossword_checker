1d76bb2812b6689ff0764bada981373d4b11942f
Bert Wagner <bertwagner@bertwagner.com>
2021-12-29 06:14:31 -0500

    add remaining images counter

--- app/data_labeler/app.py
+++ app/data_labeler/app.py
@@ -1,54 +1,76 @@
 from flask import Flask, render_template, send_from_directory, request, redirect
 from pathlib import Path
 import os
+import shutil
 
 app = Flask(__name__)
 
+PROJECT_DIR = Path(__file__).resolve().parents[2]
+INPUT_IMAGES = "ml_model/data/interim/image_uploads/letters"
+OUTPUT_IMAGES = "ml_model/data/processed/image_uploads/letters"
+
 @app.route("/")
 def label_data():
-    images_to_label = __identify_unverified_files("ml_model/data/interim/image_uploads/letters","ml_model/data/processed/image_uploads/letters")
-    
-    return render_template("label_data.html",image_list = images_to_label[:10])
+    images_to_label = __identify_unverified_files(INPUT_IMAGES,OUTPUT_IMAGES)
+    short_list = images_to_label[:10]
+    images_remaining = len(images_to_label)
+    return render_template("label_data.html",image_list = short_list, images_remaining = images_remaining)
 
 
 # We use this hack to serve images from outside the flask project directory
 @app.route('/img/<path:path>')
 def send_img(path):
-    project_dir = Path(__file__).resolve().parents[2]
-    input_images = os.path.join(project_dir,"ml_model/data/interim/image_uploads/letters")
+    input_images = os.path.join(PROJECT_DIR,"ml_model/data/interim/image_uploads/letters")
     return send_from_directory(input_images, path)
 
 @app.route('/save_labels', methods=['post'])
 def save_labels():
     items = request.form
 
-    for item in items:
-        print("new item")
-        print(item)
-        print(request.form[item])
-        # each item comes in as original_1 and then next item is corrected_1
+    # Build a dictionary from the form data that is easier to work with
+    labeled_data = {}
 
-    # TODO: move files whose labels have changed
+    for item in items:
+        field_name = item.split("__")[0]
+        index = item.split("__")[1]
+        value = request.form[item]
+
+        if f"data_{index}" not in labeled_data:
+            labeled_data[f"data_{index}"] = {}
+        
+        labeled_data[f"data_{index}"][field_name] = value
+
+    # Copy files to final location
+    
+    for key,value in labeled_data.items():
+
+        __make_folder_if_not_exists(os.path.join(PROJECT_DIR,OUTPUT_IMAGES,value['corrected_label']),False)
+
+        src = os.path.join(PROJECT_DIR,INPUT_IMAGES,value['original_label'],value['original_filename'])
+        dst = os.path.join(PROJECT_DIR,OUTPUT_IMAGES,value['corrected_label'],value['original_filename'])
+        #print(f"Moving {src} to {dst}")
+        shutil.copyfile(src,dst)
 
     return redirect('/')
 
 def __identify_unverified_files(input,output):
-    project_dir = Path(__file__).resolve().parents[2]
-    
-    # Get a list of files in all subdirectories of input and output paths
-    input_files = __flatten_directories(os.path.join(project_dir,input))
-    output_files = __flatten_directories(os.path.join(project_dir,output))
 
-    print(len(input_files))
+    # Get a list of files in all subdirectories of input and output paths
+    input_files = __flatten_directories(os.path.join(PROJECT_DIR,input))
+    output_files = __flatten_directories(os.path.join(PROJECT_DIR,output))
+
     # Delete any input files already present somewhere in the output directory
+    to_verify=input_files.copy()
+
     for input_item in input_files:
         for output_item in output_files:
             if input_item['filename'] == output_item['filename']:
-                input_files.remove(input_item)
+                print(f"Removing {output_item['filename']}")
+                to_verify.remove(input_item)
                 break
     
     # At this point, all items in the input_files array still need to be labelled/verified
-    return input_files
+    return to_verify
 
 # Traveerse a directory (and subdirectories) and return in a flattened list structure
 def __flatten_directories(directory):
@@ -62,5 +84,14 @@ def __flatten_directories(directory):
 
     return input_files
 
+def __make_folder_if_not_exists(folder_path,delete_files):
+    path_exists = os.path.exists(folder_path)
+
+    if delete_files and path_exists:
+            shutil.rmtree(folder_path)
+            os.makedirs(folder_path)
+
+    if not path_exists:
+        os.makedirs(folder_path)
 
 
